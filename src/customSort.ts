dfa08b35e61d9705647d18c557272eec8301bca1
ivanapizzo <ivb.pizzo@gmail.com>
2025-10-01 13:02:39 +0200

    correccion sintaxis variable customSort function

--- src/customSort.ts
+++ src/customSort.ts
@@ -1,17 +1,17 @@
 
 export function customSort(arr: number[]): number[] {
 
-  let successfull = true;
+  let unSuccessfull = true;
 
-  while(successfull) {
-    successfull = false;
+  while(unSuccessfull) {
+    unSuccessfull = false;
     for (let i = 0; i <arr.length; i++){
       if (arr[i] > arr[i+1]){
         let auxiliar = arr[i]; // aux es 0 
         arr[i] = arr[i + 1];
         arr[i + 1] = auxiliar;
 
-          successfull = true;
+         unSuccessfull = true;
       }
     }
   }
